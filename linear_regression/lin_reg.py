9963e95cf9f9575d8264a2cb614e8fe2f53e847c
Mohamadamin Tavakoli <mohamadt@ics.uci.edu>
2020-12-06 19:14:02 -0800

    updating linear regression models

--- linear_regression/lin_reg.py
+++ linear_regression/lin_reg.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+import keras
 from keras.layers import Input, Conv2D, Flatten, Dense
 from keras.layers import Reshape, Concatenate, Add
 from keras.models import Model
@@ -11,55 +12,65 @@ import sys
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from ssim_loss import *
+#from ssim_loss import *
+
+from keras.initializers import Initializer, Constant
+import keras.backend as K
 
 os.environ["CUDA_VISIBLE_DEVICES"]="2"
 
 
+class AlphaInit(Initializer):
+    def __init__(self, alpha, **kwargs):
+        super(AlphaInit, self).__init__(**kwargs)
+        self.constant = alpha
+    def __call__(self):
+        return K.constant(self.constant)
+
 class Mult(layers.Layer):
-    def __init__(self, initializer="he_normal", **kwargs):
+    def __init__(self, init, **kwargs):
         super(Mult, self).__init__(**kwargs)
-        self.initializer = keras.initializers.get(initializer)
+        self.init = init
 
     def build(self, input_shape):
         output_dim = input_shape
-        self.kernel = self.add_weight(
-            shape=[1],
-            initializer=self.initializer,
-            name="kernel",
-            trainable=True,
-        )
-
+        if self.init == "constant":
+            self.kernel = self.add_weight(
+                shape=[1],
+                initializer=Constant(0.5),
+                name="kernel",
+                trainable=True,
+            )
+        else:
+            self.kernel = self.add_weight(
+                shape=[1],
+                initializer=keras.initializers.get("he_normal"),
+                name="kernel",
+                trainable=True,
+            )
     def call(self, inputs):
         out = inputs*self.kernel
         return out
 
     def get_config(self):
         # Implement get_config to enable serialization. This is optional.
-        base_config = super(Mult, self).get_config()
-        config = {"initializer": keras.initializers.serialize(self.initializer)}
-        return dict(list(base_config.items()) + list(config.items()))
+        config = super(Mult, self).get_config()
+        config.update({"init": self.init})
+        return dict(list(config.items()))
 
 
 def build_model(ssim=False):
     """
     Build Keras model with different hyperparameters
-
-    Note that adding dense_dim substantially increases network size.
     """
-    # perform convolution on input images twice
-    prev_w = tf.Variable(0.5)
-    next_w = tf.Variable(0.5)
     
     input_prev = Input(shape=(48, 48, 1))
 
     input_next = Input(shape=(48, 48, 1))
 
-    x_prev = Mult()(input_prev)
-    x_next = Mult()(input_next)  
+    x_prev = Mult("he")(input_prev)
+    x_next = Mult("he")(input_next)  
 
-    #x_prev = conv1(x_prev)
-    #x_next = conv1(x_next)
 
     h = Add()([x_prev, x_next])
 
@@ -84,11 +95,6 @@ def train(model, x_train, y_train, model_name):
 
 if __name__=="__main__":
     import h5py
-    # we can do grid search here
-    
-  
-    
-
 
     train_file = sys.argv[1]
 
@@ -100,11 +106,9 @@ if __name__=="__main__":
     shape_train = x_train.shape[0]
     shape_train = int(shape_train/64.)*64 # ssim needs to have the same batch size for all the mini batches
     x_train = x_train[:shape_train,:,:,:]
-    y_train = f['Y_train']
+    y_train = f['y_train']
     y_train = np.array(y_train)
     y_train = y_train[:shape_train,:,:]
-    
-    
     
     model = build_model()
     train(model, x_train, y_train, model_name)
